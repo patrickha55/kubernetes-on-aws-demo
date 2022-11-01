eaf0bbec3229defcd636c3425ce68824d861c0dc
PatrickHa <pattrickha@gmail.com>
2022-11-01 18:49:21 +0700

    Trigger travis

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+// Make this for travis
 async function main() {
   while(true) {
     console.log('Containers rule!');
